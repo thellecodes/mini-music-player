4ebf02d7a0ae47f94ce7b757f80e5929ed450d99
Samuel Anthony <51282543+samdsg@users.noreply.github.com>
2022-05-22 04:17:41 -0700

    fullscreen music player with HTML CSS and JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-
-
-/*Buttons */
 const playBtn = document.querySelector(".play"),
     skipForward = document.querySelector(".skip-forward"),
     skipBack = document.querySelector(".skip-back"),
@@ -19,55 +16,42 @@ const playBtn = document.querySelector(".play"),
 
 
 
+this.tracks = [
+    {
+        name: "Tech House vibes",
+        artist: "Artist 1",
+        cover: "https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?crop=entropy&cs=tinysrgb&fm=jpg",
+        source: "https://assets.mixkit.co/music/download/mixkit-tech-house-vibes-130.mp3",
+    },
+    {
+        name: "Hip Hop 02",
+        artist: "Artist 2",
+        cover: "https://images.unsplash.com/photo-1485579149621-3123dd979885?crop=entropy&cs=tinysrgb&fm=jpg",
+        source: "https://assets.mixkit.co/music/download/mixkit-hip-hop-02-738.mp3",
+    },
+    {
+        name: "Dreaming Big",
+        artist: "Artist 3",
+        cover: "https://images.unsplash.com/photo-1516280440614-37939bbacd81?crop=entropy&cs=tinysrgb&fm=jpg",
+        source: "https://assets.mixkit.co/music/download/mixkit-dreaming-big-31.mp3",
+    },
+];
+
+// Initial state values
 let audio = null,
-    circleLeft = null,
     barWidth = null,
     duration = null,
     currentTime = null,
     isTimerPlaying = false,
-    currentTrackIndex = 0;
+    currentTrackIndex = 0,
+    currentTrack = tracks[0];
 
-this.tracks = [
-    {
-        name: "Mekanın Sahibi",
-        artist: "Norm Ender",
-        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
-        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
-    },
-    {
-        name: "Everybody Knows",
-        artist: "Leonard Cohen",
-        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
-        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
-    },
-    {
-        name: "Extreme Ways",
-        artist: "Moby",
-        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
-        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
-    },
-    {
-        name: "Butterflies",
-        artist: "Sia",
-        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
-        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
-    },
-    {
-        name: "The Final Victory",
-        artist: "Haggard",
-        cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
-        source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
-    },
-];
-
-// when the dom is loaded
-let = currentTrack = tracks[0];
+// Set initial state values
 audio = new Audio();
 audio.src = currentTrack.source;
 img.src = currentTrack.cover;
 title.innerText = currentTrack.name;
 singer.innerText = currentTrack.artist;
-
 
 playBtn.addEventListener('click', () => {
     if (audio.paused) {
@@ -79,12 +63,29 @@ playBtn.addEventListener('click', () => {
     }
 });
 
+progressBarContainer.addEventListener('click', (x) => {
+    let maxduration = audio.duration;
+    let position = x.pageX - progressBarContainer.offsetLeft;
+    let percentage = (100 * position) / progressBarContainer.offsetWidth;
+    if (percentage > 100) percentage = 100;
+    if (percentage < 0) percentage = 0;
+    barWidth = percentage + "%";
+
+    audio.currentTime = (maxduration * percentage) / 100;
+    progressBar.style.width = `${barWidth}%`;
+    progressHead.style.setProperty("left", `${barWidth}%`);
+    img.src = currentTrack.cover;
+});
+
+
 skipForward.addEventListener('click', () => {
+
     if (currentTrackIndex < tracks.length - 1) {
         currentTrackIndex++;
     } else {
         currentTrackIndex = 0;
     }
+
     currentTrack = tracks[currentTrackIndex];
 
     audio.src = currentTrack.source;
@@ -141,23 +142,6 @@ skipBack.addEventListener('click', () => {
     }, 300);
 });
 
-progressBarContainer.addEventListener('click', (x) => {
-    let maxduration = audio.duration;
-    let position = x.pageX - progressBarContainer.offsetLeft;
-    let percentage = (100 * position) / progressBarContainer.offsetWidth;
-    if (percentage > 100) percentage = 100;
-    if (percentage < 0) percentage = 0;
-    barWidth = percentage + "%";
-
-    audio.currentTime = (maxduration * percentage) / 100;
-    progressBar.style.width = `${barWidth}%`;
-    progressHead.style.setProperty("left", `${barWidth}%`);
-    img.src = currentTrack.cover;
-
-    // audio.play();
-    isTimerPlaying = true;
-});
-
 audio.ontimeupdate = function () {
     if (audio.duration) {
         barWidth = (100 / audio.duration) * audio.currentTime;
@@ -202,7 +186,6 @@ audio.onended = function () {
 
 // Animations
 TweenMax.from('.img', 4, { rotation: "+=360", transformOrigin: "50% 50%", ease: Linear.easeNone, repeat: -1 });
-
 gsap.from("body, h1, .audio-img, .audio-title, .audio-singer, .audio-btns", {
     opacity: 0,
     duration: 2,
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Calculator App Using React JS
+
+This project was created by [Samuel Anthony](https://twitter.com/sam1an).
+
+### Inspiration
+[![Calculator App using React JS](https://assets.materialup.com/uploads/9cb29aab-d4a8-458b-ac12-1225fd97d745/preview.png)](https://www.uplabs.com/posts/music-player-app-eb82fce3-d699-4da6-b25c-47a51b7670ee)
+
+# Youtube Video - Full project video
+
+[![Fullscreen Music Player](https://www.youtube.com/watch?v=Dy4qkxdN7jY)](https://www.youtube.com/watch?v=Dy4qkxdN7jY "Fullscreen Music Player Using HTML, CSS & JavaScript")
+
+## To install
+
+Run `git clone https://github.com/thellecodes/mini-music-player.git` to grab this project from github
+
+
